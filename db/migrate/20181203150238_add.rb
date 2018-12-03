3f725917d7a0be786c473578f82a467ac26b0d9b
Pantondev <petersmann78@gmail.com>
2018-12-03 16:09:29 +0100

    added colour field

---
+++ db/migrate/20181203150238_add.rb
@@ -0,0 +1,5 @@
+class Add < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :products, :color, :colour
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_162502) do
+ActiveRecord::Schema.define(version: 2018_12_03_150238) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
