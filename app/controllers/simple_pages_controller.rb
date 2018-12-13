3793b09d201a1db189638b35c6390b6e022114bb
Pantondev <petersmann78@gmail.com>
2018-12-13 11:45:43 +0100

    5.3 mailers

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,12 +7,12 @@ class SimplePagesController < ApplicationController
   end
 
   def thank_you
-     @name = params[:name]
-     @email = params[:email]
-     @message = params[:message]
-     ActionMailer::Base.mail(from: @email,
-         to: 'petersmann78@gmail.com',
-         subject: "A new contact form message from #{@name}",
-         body: @message).deliver_now
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'petersmann78@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
    end
 end
