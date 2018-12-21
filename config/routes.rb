cd74ade3e85e5cde102cd89fda163833ccfac9c9
Pantondev <petersmann78@gmail.com>
2018-12-21 17:08:12 +0100

    5.8 Comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,26 @@
-Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
-  resources :users
-  resources :products do
-  resources: comments
+  Rails.application.routes.draw do
+    devise_for :users
+
+    resources :users
+    resources :products do
+    resources :comments
+  end # Makes comments resources are a subset of products.
+
+    get 'simple_pages/about'
+    get 'simple_pages/contact'
+    get 'simple_pages/index'
+    get 'simple_pages/landing_page'
+    get 'products/index'
+    get 'products/new'
+    get 'products/edit'
+    get 'products/show'
+    get 'orders/index'
+    get 'orders/show'
+    post 'simple_pages/thank_you'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+    root 'simple_pages#landing_page'
+
+      resources :orders, only: [:index, :show, :create, :destroy]
   end
-  resources :orders, only: [:index, :show, :create, :destroy]
-  get 'simple_pages/about'
-  get 'simple_pages/contact'
-  get 'simple_pages/index'
-  get 'simple_pages/landing_page'
-  get 'products/index'
-  get 'products/new'
-  get 'products/edit'
-  get 'products/show'
-  get 'orders/index'
-  get 'orders/show'
-  post 'simple_pages/thank_you'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
-end
