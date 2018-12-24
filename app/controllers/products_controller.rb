4f2bb085674a2cd4ab349805e975e4fcb174d345
Pantondev <petersmann78@gmail.com>
2018-12-24 12:58:49 +0100

    5.9 validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -68,7 +68,7 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
+ private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,21 @@
 class Product < ApplicationRecord
   validates :name, presence: true #Adds validation to the Product model
-
   has_many :orders
   has_many :comments
+end
+
   def self.search(search_term)
       Product.where("name LIKE ?", "%#{search_term}%")
   end
+
   def highest_rating_comment
     comments.rating_desc.first
   end
+
   def lowest_rating_comment
     comments.rating_asc.first
   end
+  
   def average_rating
   comments.average(:rating).to_f
   end
-end
