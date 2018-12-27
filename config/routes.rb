764efc5bdb7c55790a6f96588ee475e06b3d5b8c
Pantondev <petersmann78@gmail.com>
2018-12-27 17:03:51 +0100

    5.11 automated e-mail

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,6 @@
     get 'simple_pages/contact'
     get 'simple_pages/index'
     get 'simple_pages/landing_page'
-    get 'products/index'
-    get 'products/new'
-    get 'products/edit'
-    get 'products/show'
-    get 'orders/index'
-    get 'orders/show'
     post 'simple_pages/thank_you'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
