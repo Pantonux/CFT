6e87963fefecb8654419175e0c206fee8bc8cb05
Pantondev <petersmann78@gmail.com>
2018-12-28 17:10:57 +0100

    6.2 unit tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 describe Comment do
     context "when the product has comments" do
-        let(:user){ Comment.create(:user) }
+        let(:user) {User.create!(email:"info@berlin-s-bikes.com", password:"Passwordbsb")}
         let(:product){ Product.create!(name: "Cownie")}
+        let(:comment) {Comment.create(user_id: user.id, product_id: product.id, body: "This is a comment")}
 
-        before do
-            product.comments.create!(body: "This is a comment.")
-
-        it "not valid without a rating"
+        it "not valid without a rating" do
           comment.rating = nil
           expect(comment).to_not be_valid
         end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe Product do
-   let(:product) {Product.create!(name:"Cownie", description:"Cool children bike", image_url:"about.overview_1.jpg" )}
+   let(:product) {Product.create!(name:"Cownie", description:"Cool children bike", image_url:"Cownie.jpg" )}
 
-   let(:user) {User.create!(email:"julia@B.com", password:"123456")}
+   let(:user) {User.create!(email:"info@berlin-s-bikes.com", password:"Passwordbsb")}
    before do
      product.comments.create!(rating:1, user:user, body:"Awful bike")
      product.comments.create!(rating:3, user:user, body:"Ok bike")
